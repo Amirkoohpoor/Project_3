d1744aa96eb5b67e2d236d49a74daa637f25c0dc
Mugu T <ttmugu@gmail.com>
2020-02-09 13:22:24 -0500

    Revert "Revert "Merge pull request #20 from Amirkoohpoor/javascript""
    This reverts commit a1bab698471ab763b57290f6f12fbafb32f7ee3c.

--- static/js/data.js
+++
@@ -1,8 +0,0 @@
-var params = (new URL(document.location)).search;
-
-console.log("params are ", params);
-
-fetch(`/api/real-estate-search-results${params}`)
-.then(response => response.json())
-.then(response => buildDataPage(response.posting))
-.catch(err=> console.error(err.stack));
