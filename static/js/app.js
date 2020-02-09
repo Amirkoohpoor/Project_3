1c7ab2236c272f7a6c6050bb527ceea051664e1b
Mugu T <ttmugu@gmail.com>
2020-02-09 13:22:53 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Merge pull request #20 from Amirkoohpoor/javascript"""""""
    This reverts commit fd7c0e0f0a9a92e56c6424d03cd2e5322d7256c4.

--- static/js/app.js
+++ static/js/app.js
@@ -1,37 +1,3 @@
-function load_house(){
-    
-    fetch(`/api/house-full-list`)
-    .then(response => response.json())
-    .then(response => house_data(response.posting))
-    .catch(err=> console.error(err.stack));
-
-}
-//var h_data 
-var houseMarkers = [];
-
-function house_data(h_data)
-{
-        //h_data = JSON.stringify(h_data)
-        // Loop through the house lat long array
-        for (var index = 0; index < h_data.length; index++) {
-
-          //var houseMarker = [h_data[index]['Lat'], h_data[index]['Long']]
-          var houseMarker = L.marker([h_data[index]['Lat'], h_data[index]['Long']])
-          .bindPopup("<h3>" + h_data.Mls_Number + "<h3><h3>Price: " + h_data.Price + "<h3>");
-    
-        
-          // Add the marker to the bikeMarkers array
-          houseMarkers.push(houseMarker);
-        }
-
-
-  // Create a layer group made from the bike markers array, pass it into the createMap function
-  //createMap(L.layerGroup(houseMarkers));   
-    //d3.json(data, createMarkers);
-    
-var geoJSONurl = 'static/data/Data.geojson'
-
-
 var mapConfig = {
     center: [43.65, -79.38],
     zoom: 10
@@ -45,6 +11,9 @@ var tileConfig = {
   id: "mapbox.streets",
   accessToken: API_KEY
 }
+
+var geoJSONurl = 'static/data/Data.geojson'
+
 
 var map = L.map("map", mapConfig)
 var mapLayer = L.tileLayer(tileURL, tileConfig).addTo(map)
@@ -83,15 +52,4 @@ d3.json(geoJSONurl, function(data) {
                 layer.bindPopup("<h1>" + feature.properties.AREA_DESC + "</h1><hr><h2>" + feature.properties.AREA_TYPE + "</h2>");
         }
     }).addTo(map);
-})
-
-
-
-}
-
-if(window.addEventListener) {
-    window.addEventListener('load', load_house,false); //W3C
-} else {
-    window.attachEvent('onload',load_house); //IE
-}
-
+});
