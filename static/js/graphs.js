55c335b8f73ee27636e7dc17e2f27c9ecc6a3d7e
Mugu T <ttmugu@gmail.com>
2020-02-09 13:22:30 -0500

    Revert "Revert "Revert "Merge pull request #20 from Amirkoohpoor/javascript"""
    This reverts commit d1744aa96eb5b67e2d236d49a74daa637f25c0dc.

--- static/js/graphs.js
+++
@@ -1,4 +0,0 @@
-function buildGraphs(properties){
-    // enter d3 code to plot graphs here, properties is the output of the users search result in json format
-    console.log("buildGraphs", properties);
-}
