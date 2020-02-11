ff6e286ade8a26e88d0963954b77fd1f843f3327
Amirkoohpoor <54290503+Amirkoohpoor@users.noreply.github.com>
2020-02-10 20:04:12 -0500

    Update build_data_page.js

--- static/js/build_data_page.js
+++ static/js/build_data_page.js
@@ -17,51 +17,51 @@ function buildDataPage(properties) {
     var tileConfig = {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
-        id: "mapbox.streets",
+        id: "mapbox.satellite",
         accessToken: API_KEY
     }
         
-    var geoJSONurl = 'static/data/Data.geojson'
+    // var geoJSONurl = 'static/data/Data.geojson'
         
         
     var map = L.map("fmap", mapConfig)
     var mapLayer = L.tileLayer(tileURL, tileConfig).addTo(map)
-    d3.json(geoJSONurl, function(data) {
-        L.geoJSON(data, {
-            style: function(feature) {
-                return {
-                    color: "blue",
-                    fillColor: "salmon",
-                    fillOpacity: 0.5,
-                    weight: 1.5
-                    };
-            }, 
-            onEachFeature : function (feature, layer) {
-                    layer.on({
-                        mouseover: function(event) {
-                            layer = event.target, 
-                            layer.setStyle({
-                            fillOpacity: 0.75
-                            });
-                        }, 
-                        doubleclick: function(event) {
-                            //map.fitBounds(event.target.getBounds());
-                            map.zoomOut();
-                        },
-                        click: function (event) {
-                            map.zoomIn();
-                        },
-                        mouseout: function(event) {
-                            layer = event.target, 
-                            layer.setStyle({
-                                fillOpacity: 0.5
-                            })
-                        }
-                    });
-                    layer.bindPopup("<h1>" + feature.properties.AREA_DESC + "</h1><hr><h2>" + feature.properties.AREA_TYPE + "</h2>");
-            }
-        }).addTo(map);
-    });
+    // d3.json(geoJSONurl, function(data) {
+    //     L.geoJSON(data, {
+    //         style: function(feature) {
+    //             return {
+    //                 color: "blue",
+    //                 fillColor: "salmon",
+    //                 fillOpacity: 0.5,
+    //                 weight: 1.5
+    //                 };
+    //         }, 
+    //         onEachFeature : function (feature, layer) {
+    //                 layer.on({
+    //                     mouseover: function(event) {
+    //                         layer = event.target, 
+    //                         layer.setStyle({
+    //                         fillOpacity: 0.75
+    //                         });
+    //                     }, 
+    //                     doubleclick: function(event) {
+    //                         //map.fitBounds(event.target.getBounds());
+    //                         map.zoomOut();
+    //                     },
+    //                     click: function (event) {
+    //                         map.zoomIn();
+    //                     },
+    //                     mouseout: function(event) {
+    //                         layer = event.target, 
+    //                         layer.setStyle({
+    //                             fillOpacity: 0.5
+    //                         })
+    //                     }
+    //                 });
+        //             layer.bindPopup("<h1>" + feature.properties.AREA_DESC + "</h1><hr><h2>" + feature.properties.AREA_TYPE + "</h2>");
+        //     }
+    //     // }).addTo(map);
+    // });
     properties.forEach(function(unit){
         L.marker([unit.Lat, unit.Long]).bindPopup(
         "<h1> MLS_number:  " + unit.Mls_Number 
