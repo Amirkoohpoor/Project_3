190737bad7128abbf155a085ec5162fe425b5102
monabuttu <53659505+monabuttu@users.noreply.github.com>
2020-02-10 19:17:03 -0500

    remove app.js

--- static/js/build_data_page.js
+++ static/js/build_data_page.js
@@ -1,7 +1,6 @@
 function buildDataPage(properties) {
     // enter d3 code to plot graphs here, properties is the output of the users search result in json format
     console.log("buildDataPage", properties);
-    console.log("buildDataPage building graph to: ", d3.select("#map"));
     var selection = d3.selectAll("tbody").selectAll("tr").data(properties);
     selection.enter()
         .append("tr")
