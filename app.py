984ece82625269dddac7b249ee01bdd0ca9de45c
monabuttu <53659505+monabuttu@users.noreply.github.com>
2020-02-10 19:44:34 -0500

    change to geojson

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def fdata():
 
 @app.route('/crime')
 def crimes():
-   response = requests.get("https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/MCI_2014_to_2018/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=-79.473%2C43.723%2C-79.419%2C43.733&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=json")
+   response = requests.get("https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/MCI_2014_to_2018/FeatureServer/0/query?where=1%3D1&outFields=*&geometry=-79.473%2C43.723%2C-79.419%2C43.733&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&outSR=4326&f=geojson")
    response_json=response.json()
    return jsonify(response_json)
 
